637296df3b32751d1d6cc117412abdc2f5c320cc
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-04-16 09:46:30 -0300

    Criando o conteudo da classe CreditCard

--- src/br/com/CreditCard/bank/CreditCard.java
+++ src/br/com/CreditCard/bank/CreditCard.java
@@ -1,4 +1,23 @@
 package br.com.CreditCard.bank;
 
 public class CreditCard {
+    private float balance;
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public int payWithCredit(float price){
+        if (this.balance >= price) {
+            this.balance -= price;
+            return 1;
+        } else {
+            System.out.println("SALDO INSUFICIENTE!");
+            return 0;
+        }
+    }
 }
