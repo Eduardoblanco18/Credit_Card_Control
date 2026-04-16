5623178bef51755ad1d1aac9c46bd72986d51c05
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-04-16 09:47:34 -0300

    Criando a classe Items e seu conteúdo

---
+++ src/br/com/CreditCard/shop/Items.java
@@ -0,0 +1,22 @@
+package br.com.CreditCard.shop;
+
+public class Items {
+    private String name;
+    private String Price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+}
